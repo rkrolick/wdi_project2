853b595eaa098f3732124ad555b4811357b2a5d2
Ryan Krolick <rkrolick@gmail.com>
2016-03-21 15:26:05 -0400

    added new and show functionality for maps

--- app/controllers/factorio_maps_controller.rb
+++ app/controllers/factorio_maps_controller.rb
@@ -1,8 +1,22 @@
 class FactorioMapsController < ApplicationController
 
   def index
-    @maps = FactorioMap.all
+    @factorio_map = FactorioMap.new
+    @factorio_maps = FactorioMap.all
   end
 
-  
+  def create
+    @factorio_map = FactorioMap.create!(map_params)
+    redirect_to @factorio_map
+  end
+
+  def show
+    @factorio_map = FactorioMap.find(params[:id])
+  end
+
+  private
+  def map_params
+    params.require(:factorio_map).permit(:name)
+  end
+
 end
